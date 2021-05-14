0d2d25d75cd8defd4cb9ea05f0c7075e34d0d0c9
jdmcclur <jdmcclur@us.ibm.com>
2021-05-14 15:02:31 -0500

    formatting

--- daytrader7-frontend-service/src/main/java/com/ibm/websphere/samples/daytrader/restclient/AccountClient.java
+++ daytrader7-frontend-service/src/main/java/com/ibm/websphere/samples/daytrader/restclient/AccountClient.java
@@ -18,10 +18,10 @@ import com.ibm.websphere.samples.daytrader.entities.AccountProfileDataBean;
 
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
-@RegisterRestClient(configKey="accountClient")
+@RegisterRestClient(configKey = "accountClient")
 @Path("/")
-public interface AccountClient {  
-  
+public interface AccountClient {
+
   @Path("/getAccountData/{userId}")
   @GET
   @Produces(MediaType.APPLICATION_JSON)
@@ -44,7 +44,7 @@ public interface AccountClient {
   @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
   @Produces(MediaType.APPLICATION_JSON)
   public BigDecimal updateAccountBalance(@FormParam("userId") String userId,
-      @FormParam("balanceUpdate") BigDecimal balanceUpdate) ;
+      @FormParam("balanceUpdate") BigDecimal balanceUpdate);
 
   @Path("/login")
   @POST
@@ -66,7 +66,6 @@ public interface AccountClient {
       @FormParam("fullName") String fullName, @FormParam("address") String address, @FormParam("email") String email,
       @FormParam("creditcard") String creditcard, @FormParam("openBalance") BigDecimal openBalance);
 
-
   // DB ------
 
   @GET
@@ -78,4 +77,3 @@ public interface AccountClient {
   public Response resetDB(@PathParam("deleteAll") Boolean deleteAll);
 
 }
-
